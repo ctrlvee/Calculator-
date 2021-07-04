5ef77dfb740006b7c2a78aa53de6909505c51ecd
ctrlVee <me123opportunity@gmail.com>
2021-07-03 21:02:24 -0400

    Created basic operation functions

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(numOne, numTwo) {
+    return numOne + numTwo
+}
+function subtract(numOne, numTwo) {
+    return numOne - numTwo
+}
+function multiply(numOne, numTwo) {
+    return numOne * numTwo
+}
+function divide(numOne, numTwo) {
+    return numOne / numTwo
+}
+function compute(operator,numOne, numTwo) {
+    switch (operator) {
+        case '+':
+            return add(numOne,numTwo);
+        case 'subtract':
+            return subtract(numOne,numTwo)
+        case '*':
+            return multiply(numOne,numTwo)
+        case '/':
+            return divide(numOne,numTwo)
+    }
+}
+compute('/',2,3);
