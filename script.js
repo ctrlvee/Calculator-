24a5b7499cdd719e8fb2ecc927f1593a19ef617f
ctrlVee <me123opportunity@gmail.com>
2021-07-04 16:06:15 -0400

    Working updateDisplay() w/ compute() in console

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let result = 0;
+
+
+// Section for display
+const displayContainer = document.createElement('div');
+displayContainer.classList.add('displayContainer');
+document.body.appendChild(displayContainer);
+
+// Display current & previous container
+const currentValue = document.createElement('p')
+currentValue.classList.add('currentValue');
+//currentValue.textContent = '';
+//displayContainer.appendChild(currentValue);
+
+const previousValue = document.createElement('p');
+previousValue.classList.add('previousValue');
+previousValue.textContent = 'previous';
+displayContainer.appendChild(previousValue);
+
 function add(numOne, numTwo) {
     return numOne + numTwo
 }
@@ -13,28 +32,26 @@ function divide(numOne, numTwo) {
 function compute(operator,numOne, numTwo) {
     switch (operator) {
         case '+':
-            return add(numOne,numTwo);
-        case 'subtract':
-            return subtract(numOne,numTwo)
+            result = add(numOne,numTwo);
+            break
+        case '-':
+            result = subtract(numOne,numTwo);
+            break
         case '*':
-            return multiply(numOne,numTwo)
+            result = multiply(numOne,numTwo)
+            break
         case '/':
-            return divide(numOne,numTwo)
+            result = divide(numOne,numTwo);
+            break
     }
+    
+    updateDisplay(result);
+    console.log("I'm here");
 }
 
-// Section for display
-const displayContainer = document.createElement('div');
-displayContainer.classList.add('displayContainer');
-document.body.appendChild(displayContainer);
-
-// Display current & previous container
-const currentValue = document.createElement('p')
-currentValue.classList.add('currentValue');
-currentValue.textContent = 'current';
+function updateDisplay(result) {
+    currentValue.textContent = result;
 displayContainer.appendChild(currentValue);
-
-const previousValue = document.createElement('p');
-previousValue.classList.add('previousValue');
-previousValue.textContent = 'previous';
-displayContainer.appendChild(previousValue);
+    //document.getElementsByClassName('currentValue').textContent = result;
+   
+}
